10a3f9be7ca20737ae03e07d2287bf8cb2a46ee5
GarifullinII <ildaridrisovich@gmail.com>
2021-11-12 15:19:30 +0400

    ahanged App and Cart component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import './App.css';
+import Cart from './Cart';
 import Goods from './Goods';
 import goods from './goods.json';
 
 function App() {
 
-  const [cart, setCart] = React.useState({}); // object basket
-  const [count, setCount] = React.useState(0); // goods counter
+  const [cart, setCart] = React.useState({});
+  const [count, setCount] = React.useState(0);
+
+  function addToCart (event){
+    event.preventDefault(); 
+
+    if (!event.target.classList.contains('add-to-cart')) return false;
+
+    const k = event.target.dataset.key;
+    if (cart[k] === undefined) {
+      cart[k] = 1;
+    }
+    else {
+      cart[k]++;
+    }
+
+    setCart(cart);
+
+    setCount(count + 1);
+}
 
   return (
     <div className="container">
       <h1>
         Cart
       </h1>
-      <div className="goods-field">
+      <div className="goods-field" onClick={addToCart}>
         {
-          goods.map(item => <Goods articul={item.articul} title={item.title} cost={item.cost} image={item.image} />)
+          goods.map(item => <Goods articul={item.articul} title={item.title} cost={item.cost} image={item.image} key={item.articul}/>)
         }
       </div>
+      <Cart goods={goods} cart={cart} />
     </div>
   );
 }
--- src/Cart.js
+++ src/Cart.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
-const Cart = () => {
+const Cart = (props) => {
+
+    const cartKeys = Object.keys(props.cart);
+    // индексируем goods
+    const goodsObj = props.goods.reduce((accum,item) => {
+        accum[item.articul] = item;
+        return accum;
+    }, {});
+
+
 	return (
-		<>
-		</>
+		<div className="cart-field">
+			<h1>Корзина</h1>
+			<table>
+				<tbody>
+				<tr><th>Art</th><th>Count</th><th>Cost</th></tr>        
+				{
+					cartKeys.map(item =>
+						<tr key={item}>
+							<td>{goodsObj[item]['title']}</td>
+							<td>{props.cart[item]}</td>
+							<td>{goodsObj[item]['cost']*props.cart[item]}</td>
+						</tr>  
+                    )
+                }
+				</tbody>
+			</table>
+		</div>	
 	)
 }
 
